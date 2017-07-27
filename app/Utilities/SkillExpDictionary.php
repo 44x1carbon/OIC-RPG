94161bda8059f0e6091c9282b3cc1e4a65256977
44x1carbon <b4079@oic.jp>
2017-07-27 17:34:22 +0900

    スキルLv上限取得メソッド実装

--- app/Utilities/SkillExpDictionary.php
+++ app/Utilities/SkillExpDictionary.php
@@ -15,4 +15,10 @@ class SkillExpDictionary
         if(!array_has(self::$table, $level)) throw new \Exception("Undefined offset");
         return self::$table[$level];
     }
+
+    public static function maxLevel(): int
+    {
+        $array = array_keys(self::$table);
+        return max($array);
+    }
 }
