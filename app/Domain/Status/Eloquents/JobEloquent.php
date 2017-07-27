eb57d827633848e1d12a5f636a00387cae896ac5
44x1carbon <b4079@oic.jp>
2017-07-27 18:21:20 +0900

    VO,Entityへの変換実装

--- app/Domain/Status/Eloquents/JobEloquent.php
+++ app/Domain/Status/Eloquents/JobEloquent.php
@@ -2,14 +2,36 @@
 
 namespace App\Domain\Status\Eloquents;
 
+use App\Domain\Status\Entity\Job;
+use App\Domain\Status\ValueObject\JobInfo;
 use Illuminate\Database\Eloquent\Model;
 
 class JobEloquent extends Model
 {
     protected $table = "jobs";
+    protected $guarded = [];
 
     public function requiredSkills()
     {
         $this->belongsToMany(SkillEloquent::class, "required_skills", "job_id", "skill_id")->withPivot("required_level");
     }
+
+    function toEntity():Job
+    {
+        $scope = [
+            Job::SCOPE_INFO => $this->toValueObject()
+        ];
+        return new Job($this->id, $scope);
+    }
+
+    function toValueObject(): JobInfo
+    {
+        return new JobInfo([
+            "jobCode" => $this->job_code,
+            "name" => $this->name,
+            "imageUrl" => $this->image_url,
+            "memo" => $this->memo
+        ]);
+    }
+
 }
