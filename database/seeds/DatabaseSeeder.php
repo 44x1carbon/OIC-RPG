61605070f80880d72fe297fc4e17dabdccbbd991
44x1carbon <b4079@oic.jp>
2017-10-03 15:25:30 +0900

    修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TestDataSeeder::class);
+        // $this->call(TestDataSeeder::class);
     }
 }
