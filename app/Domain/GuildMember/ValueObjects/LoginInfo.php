d7db4013f753935a9ef0ba7b64987b0ab28ee9b9
ni-hara <quale7@gmail.com>
2017-10-24 17:28:51 +0900

    Login情報保持クラスの作成

--- app/Domain/GuildMember/ValueObjects/LoginInfo.php
+++ app/Domain/GuildMember/ValueObjects/LoginInfo.php
@@ -8,8 +8,29 @@
 
 namespace App\Domain\GuildMember\ValueObjects;
 
+use App\Domain\GuildMember\ValueObjects\MailAddress;
+use App\Domain\GuildMember\ValueObjects\PassWord;
+use PhpParser\Node\Scalar\String_;
+
 
 class LoginInfo
 {
+    private $address;
+    private $password;
 
+    public function __construct(MailAddress $address, PassWord $passWord)
+    {
+        $this->address = $address;
+        $this->password = $passWord;
+    }
+
+    public function address(): MailAddress
+    {
+        return $this->address;
+    }
+
+    public function password(): PassWord
+    {
+        return $this->password;
+    }
 }
