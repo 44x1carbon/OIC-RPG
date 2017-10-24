ac584ba54e020d971b24021dc5434966cd475de1
ymgn <b4183@oic.jp>
2017-10-24 17:57:02 +0900

    add 活動時間(ActivityPeriod)のVOとSpecを追加

---
+++ app/Domain/Party/ValueObjects/ActivityPeriod.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yamagon
+ * Date: 2017/10/24
+ * Time: 15:59
+ */
+
+namespace App\Domain\Party\ValueObjects;
+
+use App\Domain\Party\Spec\ActivityPeriodSpec;
+
+class ActivityPeriod
+{
+
+    private $timeStamp;
+
+    public function __construct(int $timeStamp)
+    {
+        $this->timeStamp = $timeStamp;
+        if( !ActivityPeriodSpec::isUnixTimeFormat($this->timeStamp) ) throw new \Exception("Error");
+    }
+
+    public function timeStamp()
+    {
+        return $this->timeStamp;
+    }
+
+    public  function getIso8601()
+    {
+        return date(DATE_ISO8601,$this->timeStamp);
+    }
+
+}
