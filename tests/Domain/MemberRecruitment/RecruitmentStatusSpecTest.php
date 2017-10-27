42f97c8129d00e3242d6e0aa084cf8102f88688c
ymgn <b4183@oic.jp>
2017-10-27 15:42:06 +0900

    add RecruitmentStatusSpecのテストを追加

---
+++ tests/Domain/MemberRecruitment/RecruitmentStatusSpecTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yamagon
+ * Date: 2017/10/27
+ * Time: 15:20
+ */
+
+namespace Tests\Domain\MemberRecruitment;
+
+
+use App\Domain\MemberRecruitment\ValueObjects\RecruitmentStatus;
+use Tests\TestCase;
+
+class RecruitmentStatusSpecTest extends TestCase
+{
+    function testSuccess()
+    {
+        $status = 'open';
+        new RecruitmentStatus($status);
+        $status = 'close';
+        new RecruitmentStatus($status);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    function testFail()
+    {
+        $status = 'comming';
+        new RecruitmentStatus($status);
+    }
+}
