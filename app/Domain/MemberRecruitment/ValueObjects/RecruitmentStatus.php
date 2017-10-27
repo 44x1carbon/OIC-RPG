a262c569cc530ddaeb78bdee10f1368167e0f3b4
ymgn <b4183@oic.jp>
2017-10-27 15:43:20 +0900

    add VOのRecruitmentStatusを実装

---
+++ app/Domain/MemberRecruitment/ValueObjects/RecruitmentStatus.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yamagon
+ * Date: 2017/10/27
+ * Time: 14:49
+ */
+
+namespace App\Domain\MemberRecruitment\ValueObjects;
+
+
+use App\Domain\MemberRecruitment\Spec\RecruitmentStatusSpec;
+use App\Exceptions\DomainException;
+
+class RecruitmentStatus
+{
+    const OPEN = 'open';
+    const CLOSE = 'close';
+    const STATUS_LIST = [self::OPEN, self::CLOSE];
+
+    private $status;
+
+    public function __construct(String $status)
+    {
+        $this->status = $status;
+        if( !RecruitmentStatusSpec::isAvailable($this->status) ) throw new DomainException("Error");
+    }
+
+}
