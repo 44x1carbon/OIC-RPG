bfd1eeef97c21935b382232a6c84f5ba1a79b509
ymgn <b4183@oic.jp>
2017-10-31 13:17:07 +0900

    add MemberRecruitmentを実装 ・Entity ・RepositoryImpl ・RepositoryInterface ・Factory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Domain\Course\RepositoryInterface\CourseRepositoryInterface;
+use App\Domain\MemberRecruitment\RepositoryInterface\MemberRecruitmentRepositoryInterface;
 use App\Domain\ProductionType\RepositoryInterface\ProductionTypeRepositoryInterface;
 use App\Domain\GuildMember\RepositoryInterface\GuildMemberRepositoryInterface;
 use App\Infrastracture\Course\CourseOnMemoryRepositoryImpl;
+use App\Infrastracture\MemberRecruitment\MemberRecruitmentOnMemoryRepositoryImpl;
 use App\Infrastracture\ProductionType\ProductionTypeOnMemoryRepositoryImpl;
 use App\Infrastracture\GuildMember\GuildMemberOnMemoryRepositoryImpl;
 use Illuminate\Support\ServiceProvider;
@@ -33,5 +35,6 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(CourseRepositoryInterface::class, CourseOnMemoryRepositoryImpl::class);
         $this->app->singleton(ProductionTypeRepositoryInterface::class, ProductionTypeOnMemoryRepositoryImpl::class);
         $this->app->singleton(GuildMemberRepositoryInterface::class,GuildMemberOnMemoryRepositoryImpl::class);
+        $this->app->singleton(MemberRecruitmentRepositoryInterface::class, MemberRecruitmentOnMemoryRepositoryImpl::class);
     }
 }
