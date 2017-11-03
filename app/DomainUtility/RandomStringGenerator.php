36113f544caeb6bd82ae77429543726ccc5ef0e1
ymgn <b4183@oic.jp>
2017-11-03 13:36:37 +0900

    add 小文字の英字を使ってランダムな文字を作成するクラスを作成

---
+++ app/DomainUtility/RandomStringGenerator.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yamagon
+ * Date: 2017/11/03
+ * Time: 13:27
+ */
+
+namespace App\DomainUtility;
+
+
+class RandomStringGenerator
+{
+    /**
+     * ランダム文字列生成 (英小文字)
+     * $length: 生成する文字数
+     */
+    public static function makeLowerCase($length) {
+        static $chars = 'abcdefghijklmnopqrstuvwxyz';
+        $str = '';
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $chars[mt_rand(0, 25)];
+        }
+        return $str;
+    }
+
+}
