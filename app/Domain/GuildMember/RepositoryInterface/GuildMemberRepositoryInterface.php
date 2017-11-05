07cd60d81dead32ed9286c115c64f3d1b756ebe4
44x1carbon <b4079@oic.jp>
2017-11-05 17:06:48 +0900

    メールアドレスからGuildMemberを取得できるメソッド追加

--- app/Domain/GuildMember/RepositoryInterface/GuildMemberRepositoryInterface.php
+++ app/Domain/GuildMember/RepositoryInterface/GuildMemberRepositoryInterface.php
@@ -11,11 +11,14 @@ namespace App\Domain\GuildMember\RepositoryInterface;
 
 use App\Domain\GuildMember\GuildMember;
 use App\Domain\GuildMember\ValueObjects\LoginInfo;
+use App\Domain\GuildMember\ValueObjects\MailAddress;
 use App\Domain\GuildMember\ValueObjects\StudentNumber;
 
 interface GuildMemberRepositoryInterface
 {
     public function findByLoginInfo(LoginInfo $loginInfo): ?GuildMember;
+
+    public function findByMailAddress(MailAddress $mailAddress): ?GuildMember;
 
     public function findByStudentNumber(StudentNumber $studentNumber): ?GuildMember;
 
