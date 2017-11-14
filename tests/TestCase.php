e05e9184fb286ed5a25c2cd5a4c3863cc657bab8
44x1carbon <b4079@oic.jp>
2017-11-14 13:37:57 +0900

    テスト時のDBクリア、sqliteを利用するように変更

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate:refresh', [
+            '--seed' => true
+        ]);
+    }
+
+    public function tearDown()
+    {
+        Artisan::call('migrate:reset');
+        parent::tearDown();
+    }
 }
