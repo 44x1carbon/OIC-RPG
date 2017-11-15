50a8d7fac604fe8a0d86ffe910646ff9c8ed202d
ni-hara <quale7@gmail.com>
2017-11-16 08:09:31 +0900

    レベルアップと経験値追加クラス

---
+++ app/Domain/PossessionSkill/AddProcess.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/11/10
+ * Time: 13:07
+ */
+
+namespace App\Domain\PossessionSkill;
+
+
+class AddProcess
+{
+    const LEVEL_UP_INTERVAL = 100;
+
+    public static function addExp(PossessionSkill $beforePossessionSkill,int $exp): PossessionSkill
+    {
+        $afterPossessionSkill = clone $beforePossessionSkill;
+        $afterPossessionSkill->setTotalExp($beforePossessionSkill->totalExp() + $exp);
+
+        $levelUpValue = self::levelUp($beforePossessionSkill, $afterPossessionSkill);
+        $afterPossessionSkill->setSkillLevel($afterPossessionSkill->skillLevel() + $levelUpValue);
+
+        return $afterPossessionSkill;
+    }
+
+    public static function levelUp(PossessionSkill $beforePossessionSkill, PossessionSkill $afterPossessionSkill): int
+    {
+        $beforeTotalExp = $beforePossessionSkill->totalExp();
+        $afterTotalExp = $afterPossessionSkill->totalExp();
+        $exp = $afterTotalExp - $beforeTotalExp;
+
+        return (int) floor(($beforeTotalExp % self::LEVEL_UP_INTERVAL + $exp) / self::LEVEL_UP_INTERVAL);
+    }
+}
