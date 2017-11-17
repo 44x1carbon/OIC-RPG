da65348258a1fc1deac2fa3a62b433330d7ee726
ni-hara <quale7@gmail.com>
2017-11-17 15:58:44 +0900

    AddProcessクラスからaddExp,levelUpメソッド移植

--- app/Domain/PossessionSkill/Service/PossessionSkillDomainService.php
+++ app/Domain/PossessionSkill/Service/PossessionSkillDomainService.php
@@ -7,10 +7,12 @@ use App\Domain\GuildMember\Spec\GuildMemberSpec;
 use App\Domain\GuildMember\ValueObjects\StudentNumber;
 use App\Domain\PossessionSkill\AddProcess;
 use App\Domain\PossessionSkill\Factory\PossessionSkillFactory;
+use App\Domain\PossessionSkill\PossessionSkill;
 use App\Domain\PossessionSkill\RepositoryInterface\PossessionSkillRepositoryInterface;
 use App\Domain\PossessionSkill\Spec\AddProcessSpec;
 use App\Domain\Skill\Skill;
 use App\Exceptions\DomainException;
+use App\Infrastracture\PossessionSkill\PossessionSkillOnMemoryRepositoryImpl;
 
 /**
  * Created by PhpStorm.
@@ -21,26 +23,41 @@ use App\Exceptions\DomainException;
 
 class PossessionSkillDomainService
 {
+    const LEVEL_UP_INTERVAL = 100;
+
     protected $possessionSkillRepo;
 
-    public function __construct()
+    public function __construct(PossessionSkillRepositoryInterface $repo)
     {
+        $this->possessionSkillRepo = $repo;
     }
 
-    public function addService(StudentNumber $studentNumber,Skill $skill,int $exp): bool
+    public function addService(PossessionSkill $possessionSkill, int $exp): bool
     {
-        if(!GuildMemberSpec::isExistStudentNumber($studentNumber)) return false;
-
-        $this->possessionSkillRepo = app(PossessionSkillRepositoryInterface::class);
-        $possessionSkill = $this->possessionSkillRepo->findBySkill($skill);
-        if(is_null($possessionSkill))
-        {
-            $possessSkillFactory = new PossessionSkillFactory();
-            $possessionSkill = $possessSkillFactory->possessSkill($skill);
-        }
-
-        $addResultPossessionSkill = AddProcess::AddExp($possessionSkill, $exp);
+        $addResultPossessionSkill = self::AddExp($possessionSkill, $exp);
+        $addResultPossessionSkill = self::levelUp($possessionSkill, $addResultPossessionSkill);
 
         return $this->possessionSkillRepo->save($addResultPossessionSkill);
+    }
+
+    public static function addExp(PossessionSkill $beforePossessionSkill, int $exp): PossessionSkill
+    {
+        $afterPossessionSkill = $beforePossessionSkill->clone();
+        $afterPossessionSkill->setTotalExp($beforePossessionSkill->totalExp() + $exp);
+
+        return $afterPossessionSkill;
+    }
+
+    public static function levelUp(PossessionSkill $beforePossessionSkill, PossessionSkill $afterPossessionSkill): PossessionSkill
+    {
+        $beforeTotalExp = $beforePossessionSkill->totalExp();
+        $afterTotalExp = $afterPossessionSkill->totalExp();
+
+        $exp = $afterTotalExp - $beforeTotalExp;
+
+        $levelUpValue = (int) floor(($beforeTotalExp % self::LEVEL_UP_INTERVAL + $exp) / self::LEVEL_UP_INTERVAL);
+        $afterPossessionSkill->setSkillLevel($afterPossessionSkill->skillLevel() + $levelUpValue);
+
+        return $afterPossessionSkill;
     }
 }
