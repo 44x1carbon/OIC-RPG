66838f796016c330ac15467058d07b08389737db
44x1carbon <b4079@oic.jp>
2017-11-19 19:42:25 +0900

    各メソッドの実装

--- app/Infrastracture/ProductionType/ProductionTypeEloquentRepositoryImpl.php
+++ app/Infrastracture/ProductionType/ProductionTypeEloquentRepositoryImpl.php
@@ -4,22 +4,43 @@ namespace App\Infrastracture\ProductionType;
 
 use App\Domain\ProductionType\ProductionType;
 use App\Domain\ProductionType\RepositoryInterface\ProductionTypeRepositoryInterface;
+use App\Eloquents\ProductionTypeEloquent;
 
 class ProductionTypeEloquentRepositoryImpl implements ProductionTypeRepositoryInterface
 {
+    protected $eloquent;
+
+    function __construct(ProductionTypeEloquent $eloquent)
+    {
+        $this->eloquent = $eloquent;
+    }
 
     public function findById(String $id): ?ProductionType
     {
-        // TODO: Implement findById() method.
+        return safe_exec($this->eloquent->findById($id), function(ProductionTypeEloquent $value) {
+            return $value->toEntity();
+        });
+
     }
 
     public function save(ProductionType $productionType): bool
     {
-        // TODO: Implement save() method.
+        $model = $this->eloquent->findById($productionType->id());
+
+        if(is_null($model)) {
+            $model = new $this->eloquent();
+            $model->production_type_id = $productionType->id();
+        }
+
+        $model->production_type_name = $productionType->productionTypeName();
+
+        return $model->save();
     }
 
     public function all(): array
     {
-        // TODO: Implement all() method.
+        return $this->eloquent->all()->map(function(ProductionTypeEloquent $model) {
+            return $model->toEntity();
+        })->toArray();
     }
 }
