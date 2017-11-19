5b3f91c476f5233a293a99605182d0b1a89887da
44x1carbon <b4079@oic.jp>
2017-11-19 17:08:56 +0900

    ProductionIdeaのマイグレーションファイル作成

---
+++ database/migrations/2017_11_19_170006_create_production_ideas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductionIdeasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('production_ideas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('production_theme');
+            $table->unsignedInteger('production_type_id');
+            $table->text('$idea_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('production_ideas');
+    }
+}
