8b2e3a2ca814b4fc3568f4115255f011f94a30a3
44x1carbon <b4079@oic.jp>
2017-11-19 17:42:08 +0900

    toEntityメソッド追加

--- app/Eloquents/ProductionTypeEloquent.php
+++ app/Eloquents/ProductionTypeEloquent.php
@@ -2,9 +2,25 @@
 
 namespace App\Eloquents;
 
+use App\Domain\ProductionType\Factory\ProductionTypeFactory;
+use App\Domain\ProductionType\ProductionType;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductionTypeEloquent extends Model
 {
     protected $table = 'production_types';
+
+    /* @var ProductionTypeFactory $factory */
+    protected $factory;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->factory = app(ProductionTypeFactory::class);
+    }
+
+    public function toEntity(): ProductionType
+    {
+        return $this->factory->createProductionType($this->production_type_id, $this->production_type_name);
+    }
 }
