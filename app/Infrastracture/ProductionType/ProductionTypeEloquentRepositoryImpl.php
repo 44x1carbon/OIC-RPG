49ee4c91279404541cfe59a99e3181a4c78c2018
44x1carbon <b4079@oic.jp>
2017-11-19 17:19:40 +0900

    ProductionTypeのEloquentRepositoryファイル作成

---
+++ app/Infrastracture/ProductionType/ProductionTypeEloquentRepositoryImpl.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Infrastracture\ProductionType;
+
+use App\Domain\ProductionType\ProductionType;
+use App\Domain\ProductionType\RepositoryInterface\ProductionTypeRepositoryInterface;
+
+class ProductionTypeEloquentRepositoryImpl implements ProductionTypeRepositoryInterface
+{
+
+    public function findById(String $id): ?ProductionType
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function save(ProductionType $productionType): bool
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function all(): Array
+    {
+        // TODO: Implement all() method.
+    }
+}
