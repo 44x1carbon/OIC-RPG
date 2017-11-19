9f0064486607ef846e0340f9987aef180396eab9
44x1carbon <b4079@oic.jp>
2017-11-19 20:30:57 +0900

    直接生成する方法に変更

--- app/Eloquents/ProductionTypeEloquent.php
+++ app/Eloquents/ProductionTypeEloquent.php
@@ -10,18 +10,14 @@ class ProductionTypeEloquent extends Model
 {
     protected $table = 'production_types';
 
-    /* @var ProductionTypeFactory $factory */
-    protected $factory;
-
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
-        $this->factory = app(ProductionTypeFactory::class);
     }
 
     public function toEntity(): ProductionType
     {
-        return $this->factory->createProductionType($this->production_type_id, $this->production_type_name);
+        return new ProductionType($this->production_type_id, $this->production_type_name);
     }
 
     public function findById(string $id): ?ProductionTypeEloquent
