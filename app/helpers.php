41a43841dee2bf90782758888bacfc282f1dfed1
44x1carbon <b4079@oic.jp>
2017-11-20 23:04:02 +0900

    ヘルパーファイル追加&null_safetyメソッド実装

---
+++ app/helpers.php
@@ -0,0 +1,18 @@
+<?php
+
+if (! function_exists('null_safety')) {
+
+    /**
+     * 第一引数で受け取った値がnullならnullを
+     * nullでなければ第二引数で受け取った関数の第一引数に$valueを渡し、実行した結果を返す。
+     *
+     * @param $value
+     * @param callable $func
+     * @return null | mixed
+     */
+    function null_safety($value, callable $func)
+    {
+        if(is_null($value)) return $value;
+        return $func($value);
+    }
+}
