ecc92c5cbf6fc412674c11537cbf9ac5c6b4ec28
44x1carbon <b4079@oic.jp>
2017-11-21 18:38:02 +0900

    ProductionIdeaEloquentRepositoryImplの実装

--- app/Infrastracture/ProductionIdea/ProductionIdeaEloquentRepositoryImpl.php
+++ app/Infrastracture/ProductionIdea/ProductionIdeaEloquentRepositoryImpl.php
@@ -5,22 +5,43 @@ namespace App\Infrastracture\ProductionIdea;
 use App\Domain\ProductionIdea\ProductionIdea;
 use App\Domain\ProductionIdea\RepositoryInterface\ProductionIdeaRepositoryInterface;
 use App\Domain\ProductionIdea\ValueObject\ProductionIdeaId;
+use App\Eloquents\ProductionIdeaEloquent;
 
 class ProductionIdeaEloquentRepositoryImpl implements ProductionIdeaRepositoryInterface
 {
+    protected $eloquent;
+
+    function __construct(ProductionIdeaEloquent $eloquent)
+    {
+        $this->eloquent = $eloquent;
+    }
 
     public function findById(ProductionIdeaId $id): ?ProductionIdea
     {
-        // TODO: Implement findById() method.
+        return null_safety($this->eloquent->findById($id), function(ProductionIdeaEloquent $model) {
+            return $model->toEntity();
+        });
     }
 
     public function save(ProductionIdea $productionIdea): bool
     {
-        // TODO: Implement save() method.
+        $model = $this->eloquent->findById($productionIdea->id());
+        if(is_null($model)) {
+            $model = new $this->eloquent();
+            $model->production_idea_id = $productionIdea->id()->code();
+        }
+
+        $model->production_theme = $productionIdea->productionTheme();
+        $model->production_type_id = $productionIdea->productionTypeId()->code();
+        $model->idea_description = $productionIdea->ideaDescription();
+
+        return $model->save();
     }
 
     public function all(): array
     {
-        // TODO: Implement all() method.
+        return $this->eloquent->all()->map(function(ProductionIdeaEloquent $eloquent) {
+            return $eloquent->toEntity();
+        })->toArray();
     }
 }
