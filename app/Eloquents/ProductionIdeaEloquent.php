313c26dd913b4af401f3e32d0188f29791697728
44x1carbon <b4079@oic.jp>
2017-11-21 18:15:48 +0900

    findByIdメソッド実装

--- app/Eloquents/ProductionIdeaEloquent.php
+++ app/Eloquents/ProductionIdeaEloquent.php
@@ -2,9 +2,15 @@
 
 namespace App\Eloquents;
 
+use App\Domain\ProductionIdea\ValueObject\ProductionIdeaId;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductionIdeaEloquent extends Model
 {
     protected $table = 'production_ideas';
+
+    public function findById(ProductionIdeaId $id): ?ProductionIdeaEloquent
+    {
+        return $this->where('production_idea_id', $id->code())->first();
+    }
 }
