bd0f485f3857dd22a8f5133f590d42585d642e1f
44x1carbon <b4079@oic.jp>
2017-11-21 18:22:33 +0900

    不要な文字削除

--- database/migrations/2017_11_19_170006_create_production_ideas_table.php
+++ database/migrations/2017_11_19_170006_create_production_ideas_table.php
@@ -18,7 +18,7 @@ class CreateProductionIdeasTable extends Migration
             $table->string('production_idea_id');
             $table->string('production_theme');
             $table->unsignedInteger('production_type_id');
-            $table->text('$idea_description');
+            $table->text('idea_description');
             $table->timestamps();
         });
     }
