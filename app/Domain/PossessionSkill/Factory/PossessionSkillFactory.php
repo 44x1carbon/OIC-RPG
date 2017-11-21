b1e6f508daf9e3d54480a45960e0cd2d9e2f95c2
ni-hara <quale7@gmail.com>
2017-11-21 14:08:59 +0900

    一意なIDを付与する機構を作成

--- app/Domain/PossessionSkill/Factory/PossessionSkillFactory.php
+++ app/Domain/PossessionSkill/Factory/PossessionSkillFactory.php
@@ -3,7 +3,9 @@
 namespace App\Domain\PossessionSkill\Factory;
 
 use App\Domain\PossessionSkill\PossessionSkill;
+use App\Domain\PossessionSkill\RepositoryInterface\PossessionSkillRepositoryInterface;
 use App\Domain\Skill\Skill;
+use App\DomainUtility\RandomStringGenerator;
 
 /**
  * Created by PhpStorm.
@@ -14,12 +16,29 @@ use App\Domain\Skill\Skill;
 
 class PossessionSkillFactory
 {
+    protected $repo;
+
+    public function __construct()
+    {
+        $this->repo = app(PossessionSkillRepositoryInterface::class);
+    }
+
     public function createPossessionSkill(Skill $skill): PossessionSkill
     {
         $possessionSkill = new PossessionSkill();
+        $possessionSkill->setId($this->makePossessionSkillId());
         $possessionSkill->setSkill($skill);
         $possessionSkill->setSkillLevel(1);
         $possessionSkill->setTotalExp(0);
         return $possessionSkill;
+    }
+
+    public function makePossessionSkillId(): String
+    {
+        do{
+            $randId = RandomStringGenerator::makeLowerCase(4);
+        }while(!is_null($this->repo->findById($randId)));
+
+        return $randId;
     }
 }
