723ba7e4d9b2506c17eb5f88ca8fc2c2d456f217
44x1carbon <b4079@oic.jp>
2017-11-21 15:33:37 +0900

    ProductionIdeaEloquentRepositoryImplファイル作成

---
+++ app/Infrastracture/ProductionIdea/ProductionIdeaEloquentRepositoryImpl.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Infrastracture\ProductionIdea;
+
+use App\Domain\ProductionIdea\ProductionIdea;
+use App\Domain\ProductionIdea\RepositoryInterface\ProductionIdeaRepositoryInterface;
+
+class ProductionIdeaEloquentRepositoryImpl implements ProductionIdeaRepositoryInterface
+{
+
+    public function findById(String $id): ?ProductionIdea
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function save(ProductionIdea $productionIdea): bool
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function all(): Array
+    {
+        // TODO: Implement all() method.
+    }
+}
