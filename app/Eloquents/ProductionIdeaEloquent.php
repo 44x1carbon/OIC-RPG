dd2e37ad143dca9638190f2975b95bb09a9004bd
44x1carbon <b4079@oic.jp>
2017-11-21 18:22:05 +0900

    toEntityメソッド作成

--- app/Eloquents/ProductionIdeaEloquent.php
+++ app/Eloquents/ProductionIdeaEloquent.php
@@ -2,7 +2,9 @@
 
 namespace App\Eloquents;
 
+use App\Domain\ProductionIdea\ProductionIdea;
 use App\Domain\ProductionIdea\ValueObject\ProductionIdeaId;
+use App\Domain\ProductionType\ValueObject\ProductionTypeId;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductionIdeaEloquent extends Model
@@ -12,5 +14,16 @@ class ProductionIdeaEloquent extends Model
     public function findById(ProductionIdeaId $id): ?ProductionIdeaEloquent
     {
         return $this->where('production_idea_id', $id->code())->first();
+    }
+
+    public function toEntity(): ProductionIdea
+    {
+        $productionIdea = new ProductionIdea();
+        $productionIdea->setId(new ProductionIdeaId($this->production_idea_id));
+        $productionIdea->setProductionTheme($this->production_theme);
+        $productionIdea->setIdeaDescription($this->idea_description);
+        $productionIdea->setProductionTypeId(new ProductionTypeId($this->production_type_id));
+
+        return $productionIdea;
     }
 }
