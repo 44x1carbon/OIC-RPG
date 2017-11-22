02e510d7a276150e0a69bb53706371ee35a6dcde
ni-hara <quale7@gmail.com>
2017-11-22 18:56:51 +0900

    取得条件クラスの作成

---
+++ app/Domain/GetCondition/GetCondition.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/11/22
+ * Time: 18:37
+ */
+
+namespace App\Domain\GetCondition;
+
+
+use App\Domain\Skill\Skill;
+
+class GetCondition
+{
+    private $necessaryLevel;
+
+    public function __construct()
+    {
+    }
+
+    public function setSkillLevel(int $necessaryLevel)
+    {
+        $this->necessaryLevel = $necessaryLevel;
+    }
+
+    public function skillLevel(): int
+    {
+        return $this->necessaryLevel;
+    }
+}
