790043e602a17fc9c200d5339e8421bf5a9723df
ni-hara <quale7@gmail.com>
2017-11-24 11:39:46 +0900

    Skillをはやした

--- app/Domain/GetCondition/GetCondition.php
+++ app/Domain/GetCondition/GetCondition.php
@@ -13,15 +13,26 @@ use App\Domain\Skill\Skill;
 
 class GetCondition
 {
+    private $skill;
     private $necessaryLevel;
 
     public function __construct()
     {
     }
 
+    public function setSkill(Skill $skill)
+    {
+        $this->skill = $skill;
+    }
+
     public function setNecessaryLevel(int $necessaryLevel)
     {
         $this->necessaryLevel = $necessaryLevel;
+    }
+
+    public function skill(): Skill
+    {
+        return $this->skill;
     }
 
     public function necessaryLevel(): int
