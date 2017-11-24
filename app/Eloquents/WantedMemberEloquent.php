77590d0fdb3058d51b1f1991f2f88d83a9566eee
44x1carbon <b4079@oic.jp>
2017-11-24 16:57:09 +0900

    toEntityメソッド実装

--- app/Eloquents/WantedMemberEloquent.php
+++ app/Eloquents/WantedMemberEloquent.php
@@ -2,9 +2,19 @@
 
 namespace App\Eloquents;
 
+use App\Domain\WantedMember\ValueObjects\WantedStatus;
+use App\Domain\WantedMember\WantedMember;
 use Illuminate\Database\Eloquent\Model;
 
 class WantedMemberEloquent extends Model
 {
     protected $table = 'wanted_members';
+
+    public function toEntity(): WantedMember
+    {
+        $entity = new WantedMember();
+        $entity->setWantedStatus(new WantedStatus($this->wanted_status));
+
+        return $entity;
+    }
 }
