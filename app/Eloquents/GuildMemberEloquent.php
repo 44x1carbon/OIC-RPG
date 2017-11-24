87646d0f83dc1ef5188d2387a33af432d4caafa7
ymgn <b4183@oic.jp>
2017-11-24 16:00:40 +0900

    add fromEntityで永続化していないものが来た場合は、モデルを作成して返すように

--- app/Eloquents/GuildMemberEloquent.php
+++ app/Eloquents/GuildMemberEloquent.php
@@ -6,9 +6,11 @@ use App\Domain\Course\Course;
 use App\Domain\Course\RepositoryInterface\CourseRepositoryInterface;
 use App\Domain\GuildMember\Factory\GuildMemberFactory;
 use App\Domain\GuildMember\GuildMember;
+use App\Domain\GuildMember\Spec\GuildMemberSpec;
 use App\Domain\GuildMember\ValueObjects\Gender;
 use App\Domain\GuildMember\ValueObjects\MailAddress;
 use App\Domain\GuildMember\ValueObjects\StudentNumber;
+use App\Exceptions\DomainException;
 use Illuminate\Database\Eloquent\Model;
 
 class GuildMemberEloquent extends Model
@@ -37,8 +39,26 @@ class GuildMemberEloquent extends Model
 
     public function  fromEntity(GuildMember $guildMember): GuildMemberEloquent
     {
+        // Eloquentを使ってEntityから永続化されたモデルを取得する
         $guildMemberModel = $this->findByStudentNumber($guildMember->studentNumber());
-        return $guildMemberModel;
+        if ($guildMemberModel) {
+            return $guildMemberModel;
+        }
+
+        // 受け取ったEntityの項目が完全であれば、Eloquentモデルに変換して返す
+        if (GuildMemberSpec::isCompleteItem($guildMember)) {
+            $guildMemberModel = new $this;
+            $guildMemberModel->studentNumber = $guildMember->studentNumber();
+            $guildMemberModel->studentName = $guildMember->studentName();
+            $guildMemberModel->course = $guildMember->course();
+            $guildMemberModel->gender = $guildMember->gender();
+            $guildMemberModel->mailAddress = $guildMember->mailAddress();
+
+            return $guildMemberModel;
+        }else{
+            // 不完全なEntityだった場合はDomainException
+            throw new DomainException("Error");
+        }
     }
 
     public function toEntity(): GuildMember
