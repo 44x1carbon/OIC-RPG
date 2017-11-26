0d9e33e70f6ae9a1554c8fac093fd667a2ec0ba8
44x1carbon <b4079@oic.jp>
2017-11-26 23:07:27 +0900

    EloquentにtoEntityメソッド追加

--- app/Eloquents/WantedRoleEloquent.php
+++ app/Eloquents/WantedRoleEloquent.php
@@ -2,9 +2,21 @@
 
 namespace App\Eloquents;
 
+use App\Domain\WantedRole\WantedRole;
 use Illuminate\Database\Eloquent\Model;
 
 class WantedRoleEloquent extends Model
 {
     protected $table = 'wanted_roles';
+
+    public function toEntity(): WantedRole
+    {
+        $entity = new WantedRole();
+        $entity->setId($this->wanted_role_id);
+        $entity->setReferenceJobId($this->reference_job_id);
+        $entity->setName($this->role_name);
+        $entity->setRemarks($this->remarks);
+
+        return $entity;
+    }
 }
