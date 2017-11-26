821450bb31633373d93361b4a5c89d54258ba420
44x1carbon <b4079@oic.jp>
2017-11-26 22:58:18 +0900

    EloquentRepositoryで必要なものをDI

--- app/Infrastracture/WantedRole/WantedRoleEloquentRepositoryImpl.php
+++ app/Infrastracture/WantedRole/WantedRoleEloquentRepositoryImpl.php
@@ -4,13 +4,20 @@ namespace App\Infrastracture\WantedRole;
 
 use App\Domain\WantedRole\RepositoryInterface\WantedRoleRepositoryInterface;
 use App\Domain\WantedRole\WantedRole;
+use App\Eloquents\WantedRoleEloquent;
 
 class WantedRoleEloquentRepositoryImpl implements WantedRoleRepositoryInterface
 {
+    protected $eloquent;
+
+    function __construct(WantedRoleEloquent $eloquent)
+    {
+        $this->eloquent = $eloquent;
+    }
 
     public function findById(String $id): ?WantedRole
     {
-        // TODO: Implement findById() method.
+        
     }
 
     public function save(WantedRole $wantedMember): bool
