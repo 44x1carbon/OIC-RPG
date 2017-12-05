b8f8b1b63387883e8aa7370af2f9c93697bb6d78
44x1carbon <b4079@oic.jp>
2017-12-05 09:00:57 +0900

    マイグレーション作成

---
+++ database/migrations/2017_12_05_085829_create_parties_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('party_id');
+            $table->date('active_end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parties');
+    }
+}
