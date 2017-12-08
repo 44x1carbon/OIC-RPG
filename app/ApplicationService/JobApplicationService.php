6d8a54a00056d2e39ee0865005492524c14e13eb
ni-hara <quale7@gmail.com>
2017-12-08 16:04:02 +0900

    JobApplicationServiceの実装

---
+++ app/ApplicationService/JobApplicationService.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/12/05
+ * Time: 18:15
+ */
+
+namespace App\ApplicationService;
+
+
+use App\Domain\GetCondition\GetCondition;
+use App\Domain\Job\Job;
+use App\Domain\Job\JobRepositoryInterface;
+
+class JobApplicationService
+{
+    protected $jobRepository;
+
+    public function registerJob(string $jobName, string $imagePath, array $getConditions): JobId
+    {
+        $jobRepository = app(JobRepositoryInterface::class);
+        $jobId = $jobRepository->nextId();
+        $job = new Job($jobId, $jobName, $imagePath, $getConditions);
+
+        $jobRepository->save($job);
+
+        return $job->jobId();
+    }
+}
