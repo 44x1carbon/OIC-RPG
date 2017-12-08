65cd098fa78ed250e6f631ce17828c77fb35bd15
ni-hara <quale7@gmail.com>
2017-12-08 16:05:54 +0900

    Eloquentの実装

---
+++ app/Eloquents/GetConditionEloquent.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/12/08
+ * Time: 12:50
+ */
+
+namespace App\Eloquents;
+
+
+use App\Domain\Job\ValueObjects\JobId;
+use App\Domain\GetCondition\GetCondition;
+use App\Job;
+use Illuminate\Database\Eloquent\Model;
+
+class GetConditionEloquent extends Model
+{
+    protected $table = 'get_conditions';
+
+    public function findByJobId(string $jobId): ?array
+    {
+        $job = Job::find($jobId);
+        return $getConditions = $job->getCondition;
+        /*
+        $getConditionModel = $this->where('job_id', $jobId)->get();
+        return $getConditionModel->toArray();
+        */
+    }
+
+    public function findByJobIdAndSkillId(string $jobId, string $skillId): ?GetConditionEloquent
+    {
+        $getConditionModel = $this->where('job_id', $jobId)->where('skill_id', $skillId)->first();
+        return $getConditionModel;
+    }
+
+    public function fromValueObject(GetCondition $getCondition, JobId $jobId): GetConditionEloquent
+    {
+        $getConditionModel = $this->findByJobIdAndSkillId($jobId->code(), $getCondition->skillId());
+        if(is_null($getConditionModel)) $getConditionModel = new GetConditionEloquent();
+
+        $getConditionModel->job_id = $jobId->code();
+        $getConditionModel->skill_id = $getCondition->skillId();
+        $getConditionModel->required_level = $getCondition->requiredLevel();
+
+        return $getConditionModel;
+    }
+
+    public static function saveManyDomainObject(array $getConditions, JobId $jobId)
+    {
+        foreach ($getConditions as $getCondition)
+        {
+            $getConditionModel = self::fromValueObject($getCondition, $jobId);
+            $getConditionModel->save();
+        }
+    }
+}
---
+++ app/Eloquents/JobEloquent.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2017/12/08
+ * Time: 12:26
+ */
+
+namespace App\Eloquents;
+
+
+use App\Domain\Job\Job;
+use App\Domain\Job\ValueObjects\JobId;
+use Illuminate\Database\Eloquent\Model;
+
+class JobEloquent extends Model
+{
+    protected $table = 'jobs';
+
+    public function findById(string $id): ?JobEloquent
+    {
+        $jobModel = $this->where('job_id', $id)->first();
+        return $jobModel;
+    }
+
+    public function fromEntity(Job $job): JobEloquent
+    {
+        $jobModel = $this->findById($job->jobId()->code());
+        if(is_null($jobModel)) $jobModel = new JobEloquent();
+
+        $jobModel->job_id = $job->jobId()->code();
+        $jobModel->job_name = $job->jobName();
+        $jobModel->image_path = $job->imagePath();
+
+        return $jobModel;
+    }
+
+    public function toEntity(): Job
+    {
+        $jobId = new JobId($this->job_id);
+
+        $getConditionEloquent = new GetConditionEloquent();
+        $getConditions = $getConditionEloquent->findByJobId($jobId->code());
+
+        $entity = new Job(
+          $jobId,
+          $this->job_name,
+          $this->image_path,
+          $getConditions
+        );
+
+        return $entity;
+    }
+
+    public static function saveDomainObject(Job $job)
+    {
+        $jobModel = self::fromEntity($job);
+        $jobModel->save();
+    }
+}
