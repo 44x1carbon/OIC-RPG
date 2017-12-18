0665574215313064172c4688fb5ad3eb9f52ad25
ni-hara <quale7@gmail.com>
2017-12-18 18:21:03 +0900

    AppServiceProviderに加えた

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,13 @@ namespace App\Providers;
 
 use App\Domain\Course\RepositoryInterface\CourseRepositoryInterface;
 use App\Domain\GuildMember\RepositoryInterface\GuildMemberRepositoryInterface;
+use App\Eloquents\PossessionSkillEloquent;
 use App\Infrastracture\Course\CourseEloquentRepositoryImpl;
 use App\Domain\Skill\RepositoryInterface\SkillRepositoryInterface;
 use App\Infrastracture\Course\CourseOnMemoryRepositoryImpl;
 use App\Infrastracture\GuildMember\GuildMemberEloquentRepositoryImpl;
 use App\Infrastracture\GuildMember\GuildMemberOnMemoryRepositoryImpl;
+use App\Infrastracture\PossessionSkill\PossessionSkillEloquentRepositoryImpl;
 use App\Infrastracture\Skill\SkillOnMemoryRepositoryImpl;
 use App\Infrastracture\PossessionSkill\PossessionSkillOnMemoryRepositoryImpl;
 use App\Domain\PossessionSkill\RepositoryInterface\PossessionSkillRepositoryInterface;
@@ -41,6 +43,6 @@ class AppServiceProvider extends ServiceProvider
 //        $this->app->singleton(GuildMemberRepositoryInterface::class,GuildMemberOnMemoryRepositoryImpl::class);
         $this->app->singleton(GuildMemberRepositoryInterface::class,GuildMemberEloquentRepositoryImpl::class);
         $this->app->singleton(SkillRepositoryInterface::class, SkillOnMemoryRepositoryImpl::class);
-        $this->app->singleton(PossessionSkillRepositoryInterface::class, PossessionSkillOnMemoryRepositoryImpl::class);
+        $this->app->singleton(PossessionSkillRepositoryInterface::class, PossessionSkillEloquentRepositoryImpl::class);
     }
 }
