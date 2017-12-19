6fb3aa52aed1a6ce0e75b45e7187c85ec03d9bda
44x1carbon <b4079@oic.jp>
2017-12-19 12:38:05 +0900

    パーティー登録フローのルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,19 @@ Route::get('/sign_up', function() {
 });
 
 
+/** サインアップのフロー */
 Route::get('/sign_up/auth_info', GuildMemberRegistrationController::class.'@showAuthInfo')->name('show_sign_up_auth_info');
 Route::get('/sign_up/profile', GuildMemberRegistrationController::class.'@showProfile')->name('show_sign_up_profile');
 Route::get('/sign_up/school_info', GuildMemberRegistrationController::class.'@showSchoolInfo')->name('show_sign_up_school_info');
-
 Route::post('/sign_up', SignUpController::class.'@store')->name('post_sign_up');
+
+/** パーティー作成のフロー */
+Route::get('/party/registration/production_idea', PartyRegistrationController::class.'@showProductionIdea')->name('show_party_registration_production_idea');
+Route::get('/party/registration/wanted', PartyRegistrationController::class.'@showWanted')->name('show_party_registration_wanted');
+Route::get('/party/registration/confirm', PartyRegistrationController::class.'@showConfirm')->name('show_party_confirm');
+Route::post('/party', PartyController::class.'@store')->name('store_party');
 
 Route::post('/guild_member', GuildMemberController::class.'@update')->name('update_guild_member');
 
 Route::delete('/guild_member/delete', GuildMemberController::class.'@destroy')->name('destroy_guild_member');
 
-Route::post('/party', PartyController::class.'@store')->name('store_party');
