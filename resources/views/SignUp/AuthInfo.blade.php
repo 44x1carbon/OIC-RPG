ce12c03356557f8922da12f0b9321376bacbe27e
44x1carbon <b4079@oic.jp>
2017-12-19 11:54:43 +0900

    最低限のSignUp部分作成

---
+++ resources/views/SignUp/AuthInfo.blade.php
@@ -0,0 +1,9 @@
+@extends('Shared._DefaultLayout')
+
+@section('header_title')
+    AuthInfo
+@endsection
+
+@section('content')
+    <h1>AuthInfo</h1>
+@endsection
---
+++ resources/views/SignUp/SchoolInfo.blade.php
@@ -0,0 +1,9 @@
+@extends('Shared._DefaultLayout')
+
+@section('header_title')
+    SchoolInfo
+@endsection
+
+@section('content')
+    <h1>SchoolInfo</h1>
+@endsection
