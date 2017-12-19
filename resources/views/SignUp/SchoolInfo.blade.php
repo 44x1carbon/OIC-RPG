a04c9cefb6b605f41574d8b31b84d8ae8584aced
fuchi15 <sakuya815@gmail.com>
2017-12-19 14:58:37 +0900

    ギルドメンバー登録 SchoolInfo blade 作成

--- resources/views/SignUp/SchoolInfo.blade.php
+++ resources/views/SignUp/SchoolInfo.blade.php
@@ -5,5 +5,34 @@
 @endsection
 
 @section('content')
-    <h1>SchoolInfo</h1>
+    <div class="background">
+        <div class="layer layer1 map"></div>
+        <div class="layer layer2 chara"></div>
+        <div class="layer layer3 opasity"></div>
+    </div><!-- background -->
+    <div class="entry layer4">
+        <div class="entry-header header-color">
+            <h2 class="entry-title">ギルドメンバー登録</h2>
+        </div><!-- main-heading -->
+        <div class="entry-body body-color">
+            <form class="form" action="">
+                <div class="item form-item">
+                    <h3 class="form-item-title">学籍番号</h3>
+                    <input type="text" class="input" name=""><!-- 学籍番号 -->
+                </div>
+                <div class="item form-item">
+                    <h3 class="form-item-title">学年</h3>
+                        <select name="schoolyear"><!-- 学年 -->
+                            <option value="1">1年</option>
+                            <option value="2">2年</option>
+                            <option value="3">3年</option>
+                            <option value="4">4年</option>
+                        </select>
+                </div>
+                <div class="btn-wrap row flex-end-length flex-center-length">
+                    <button class="btn btn-register" type="submit">登録</button>
+                </div>
+            </form><!-- form -->
+        </div><!-- body -->
+    </div><!-- entry -->
 @endsection
