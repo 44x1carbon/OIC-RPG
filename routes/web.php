842d20fb128bbccdf72ebd8efe09f109788068da
fuchi15 <sakuya815@gmail.com>
2017-12-22 14:41:27 +0900

    パーティー管理 holding blade 作成

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,8 @@ Route::get('/sign_up', function() {
 
 /** サインアップのフロー */
 Route::get('/sign_up/auth_info', GuildMemberRegistrationController::class.'@showAuthInfo')->name('show_sign_up_auth_info');
-Route::post('/sign_up/auth_info', GuildMemberRegistrationController::class.'@doAuthInfo')->name('do_sign_up_auth_info');
 Route::get('/sign_up/profile', GuildMemberRegistrationController::class.'@showProfile')->name('show_sign_up_profile');
-Route::post('/sign_up/profile', GuildMemberRegistrationController::class.'@doProfile')->name('do_sign_up_profile');
 Route::get('/sign_up/school_info', GuildMemberRegistrationController::class.'@showSchoolInfo')->name('show_sign_up_school_info');
-Route::post('/sign_up/school_info', GuildMemberRegistrationController::class.'@doSchoolInfo')->name('do_sign_up_school_info');
 Route::post('/sign_up', SignUpController::class.'@store')->name('post_sign_up');
 
 /** パーティー作成のフロー */
@@ -42,4 +39,15 @@ Route::post('/party', PartyController::class.'@store')->name('store_party');
 Route::post('/guild_member', GuildMemberController::class.'@update')->name('update_guild_member');
 
 Route::delete('/guild_member/delete', GuildMemberController::class.'@destroy')->name('destroy_guild_member');
+
+
+/** 検索 */
+Route::get('/search', function() {
+    return view('guild.search.party');
+});
+
+/** パーティー管理 */
+Route::get('/party/management/holding', function() {
+    return view('guild.party.management.holding');
+});
 
