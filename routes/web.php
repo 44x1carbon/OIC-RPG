15c3c8c9ffc30fd8c0c76ff1f202f1dbc53c7789
fuchi15 <sakuya815@gmail.com>
2017-12-22 15:52:13 +0900

    パーティー管理 applying blade 作成

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,8 @@ Route::get('/sign_up', function() {
 
 /** サインアップのフロー */
 Route::get('/sign_up/auth_info', GuildMemberRegistrationController::class.'@showAuthInfo')->name('show_sign_up_auth_info');
-Route::post('/sign_up/auth_info', GuildMemberRegistrationController::class.'@doAuthInfo')->name('do_sign_up_auth_info');
 Route::get('/sign_up/profile', GuildMemberRegistrationController::class.'@showProfile')->name('show_sign_up_profile');
-Route::post('/sign_up/profile', GuildMemberRegistrationController::class.'@doProfile')->name('do_sign_up_profile');
 Route::get('/sign_up/school_info', GuildMemberRegistrationController::class.'@showSchoolInfo')->name('show_sign_up_school_info');
-Route::post('/sign_up/school_info', GuildMemberRegistrationController::class.'@doSchoolInfo')->name('do_sign_up_school_info');
 Route::post('/sign_up', SignUpController::class.'@store')->name('post_sign_up');
 
 /** パーティー作成のフロー */
@@ -47,4 +44,15 @@ Route::delete('/guild_member/delete', GuildMemberController::class.'@destroy')->
 /** 検索 */
 Route::get('/search', function() {
     return view('guild.search.party');
+});
+
+/** パーティー管理 */
+Route::get('/party/management/holding', function() {
+    return view('guild.party.management.holding');
+});
+Route::get('/party/management/entry', function() {
+    return view('guild.party.management.entry');
+});
+Route::get('/party/management/applying', function() {
+    return view('guild.party.management.applying');
 });
