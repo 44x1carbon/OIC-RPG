cd1ed0ff83b3403d4f75d396b4cd33b076d0c45d
44x1carbon <b4079@oic.jp>
2017-12-22 11:51:29 +0900

    コントローラにアクションメソッドを定義

--- app/Http/Controllers/PartyRegistrationController.php
+++ app/Http/Controllers/PartyRegistrationController.php
@@ -9,9 +9,29 @@ class PartyRegistrationController extends Controller
         return view('Guild.Party.Registration.ProductionIdea');
     }
 
+    public function doProductionIdea(ProductionIdeaRequest $request)
+    {
+
+    }
+
     public function showWanted()
     {
         return view('Guild.Party.Registration.Wanted');
+    }
+
+    public function handleWanted(HandleWantedRequest $request)
+    {
+
+    }
+
+    public function doWanted(WantedRequest $request)
+    {
+
+    }
+
+    public function addWantedRole(AddWantedRoleRequest $request)
+    {
+
     }
 
     public function showConfirm()
