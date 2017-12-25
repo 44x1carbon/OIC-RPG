86a5f703ef654a8ba63c1b0686e10d9084a0b795
44x1carbon <b4079@oic.jp>
2017-12-26 01:18:40 +0900

    DTOを利用してパーティ登録を実装

--- app/Presentation/PartyServiceFacade.php
+++ app/Presentation/PartyServiceFacade.php
@@ -7,6 +7,7 @@ use App\Domain\GuildMember\ValueObjects\StudentNumber;
 use App\Domain\Party\RepositoryInterface\PartyRepositoryInterface;
 use App\Domain\Party\ValueObjects\ActivityEndDate;
 use App\Domain\PartyWrittenRequest\ValueObject\WantedRoleInfo;
+use App\Presentation\DTO\PartyDto;
 use App\Presentation\DTO\WantedRoleDto;
 
 class PartyServiceFacade
@@ -18,24 +19,24 @@ class PartyServiceFacade
     }
 
     public function registerParty(
-        string $activityEndDate,
         string $studentNumber,
-        string $roleName,
-        string $productionTheme = null,
-        string $productionTypeId = null,
-        string $ideaDescription = null,
-        array $wantedRoleList = []
+        PartyDto $partyDto
     ): string
     {
-        $partyId = $this->servoce->registerParty(new ActivityEndDate($activityEndDate), new StudentNumber($studentNumber), $roleName);
+        $managerId = new StudentNumber($studentNumber);
+        $partyId = $this->servoce->registerParty(new ActivityEndDate($partyDto->activityEndDate), $managerId);
 
         $party = $this->partyRepository->findById($partyId);
-        $this->servoce->updateProductionIdea($party->id(), $productionTheme, $productionTypeId, $ideaDescription);
+        $productionIdeaDto = $partyDto->productionIdeaDto;
+        $this->servoce->updateProductionIdea($party->id(), $productionIdeaDto->productionTheme, $productionIdeaDto->productionTypeDto->id, $productionIdeaDto->ideaDescription);
 
 
-        /* @var WantedRoleDto $wantedRole */
-        foreach ($wantedRoleList as $wantedRole) {
-            $this->servoce->addWantedRole($party->id(), $wantedRole->roleName(), $wantedRole->referenceJobId(), $wantedRole->remarks(), $wantedRole->frameAmount());
+        /* @var WantedRoleDto $wantedRoleDto */
+        foreach ($partyDto->wantedRoleDtos as $wantedRoleDto) {
+            $wantedRoleId = $this->servoce->addWantedRole($party->id(), $wantedRoleDto->roleName, $wantedRoleDto->referenceJobId, $wantedRoleDto->remarks, $wantedRoleDto->frameAmount);
+            if($wantedRoleDto->managerAssigned) {
+                $party->assignMember($wantedRoleId, $managerId);
+            }
         }
 
         return $partyId;
