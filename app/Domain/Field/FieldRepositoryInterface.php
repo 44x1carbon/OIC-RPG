e7aa4d2ce5f957e22793771ca864146afba647e7
44x1carbon <b4079@oic.jp>
2017-12-28 16:20:45 +0900

    Fieldリポジトリのインターフェース作成

---
+++ app/Domain/Field/FieldRepositoryInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Domain\Field;
+
+use App\Domain\Job\ValueObjects\JobId;
+
+interface FieldRepositoryInterface
+{
+    /**
+     * @param Field $field
+     * @return bool
+     */
+    public function save(Field $field): bool;
+
+    /**
+     * 分野名からFieldインスタンスを取得する
+     *
+     * @param string $name
+     * @return Field
+     */
+    public function findByName(string $name): Field;
+
+    /**
+     * コースIDからそのコースが所属しているFieldインスタンスを取得する
+     *
+     * @param string $courseId
+     * @return Field
+     */
+    public function findByCourseId(string $courseId): Field;
+
+    /**
+     * ジョブIDからそのジョブが所属しているFieldインスタンスを取得する
+     *
+     * @param JobId $jobId
+     * @return Field
+     */
+    public function findByJobId(JobId $jobId): Field;
+
+    /**
+     * すべてのFieldインスタンスを取得する
+     *
+     * @return array
+     */
+    public function all(): array;
+}
