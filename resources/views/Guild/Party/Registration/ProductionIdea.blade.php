f026475cb37d43aa4539c2b0ca07f8a0e8d59741
fuchi15 <sakuya815@gmail.com>
2018-01-14 23:00:40 +0900

    パーティー登録 css 適用

--- resources/views/Guild/Party/Registration/ProductionIdea.blade.php
+++ resources/views/Guild/Party/Registration/ProductionIdea.blade.php
@@ -5,15 +5,14 @@
 @endsection
 
 @section('content')
-    <h1>ProductionIdea</h1>
     <div class="background">
         <div class="layer layer1 board"></div>
     </div><!-- background -->
-    <div class="entry layer2 entry-color">
+    <div class="entry layer2">
         <div class="entry-header">
             <h2 class="entry-title">制作内容</h2>
         </div><!-- entry-header -->
-        <div class="entry-body">
+        <div class="entry-body body-color party-entry-body">
             <form class="form" action="{{ route('do_party_registration_production_idea') }}" method="post">
                 {{ csrf_field() }}
                 <div class="item form-item">
@@ -22,22 +21,22 @@
                 </div><!-- item -->
                 <div class="item form-item">
                     <h3 class="form-item-title">種類</h3>
-                    <select name="party[productionIdea][productionTypeId]" id="">
+                    <select class="input select" name="party[productionIdea][productionTypeId]" id="">
                         @foreach($productionTypes as $productionType)
                             <option value="{{ $productionType->id() }}">{{ $productionType->name() }}</option>
                         @endforeach
                     </select><!-- 種類 -->
                 </div><!-- item -->
                 <div class="item form-item">
-                    <h3 class="form-item-title">説明</h3>
-                    <input type="textarea" class="input input-large" name="party[productionIdea][ideaDescription]"><!-- 説明 -->
+                    <h3 class="form-item-title">詳細</h3>
+                    <textarea class="input textarea" name="party[productionIdea][ideaDescription]"></textarea><!-- 説明 -->
                 </div><!-- item -->
                 <div class="item form-item">
                     <h3 class="form-item-title">活動期間</h3>
                     <input type="date" class="input" name="party[activityEndDate]"><!-- 活動期間 -->
                 </div><!-- item -->
                 <div class="btn-wrap row flex-end-side flex-end-length">
-                    <button class="btn btn-next" type="submit">次へ</button>
+                    <button class="btn mod-blue" type="submit">次へ</button>
                 </div><!-- btn-wrap -->
             </form><!-- form -->
         </div><!-- entry-body -->
--- resources/views/Guild/Party/Registration/Wanted.blade.php
+++ resources/views/Guild/Party/Registration/Wanted.blade.php
@@ -8,11 +8,11 @@
 <div class="background">
         <div class="layer layer1 board"></div>
     </div><!-- background -->
-    <div class="entry layer2 entry-color">
+    <div class="entry layer2 party-entry">
         <div class="entry-header">
             <h2 class="entry-title">メンバー募集内容</h2>
         </div><!-- entry-header -->
-        <div class="entry-body">
+        <div class="entry-body body-color party-entry-body">
             <form class="form" action="{{ route('do_party_registration_wanted') }}" method="post">
                 {{ csrf_field() }}
                 <?php /* @var \App\Presentation\DTO\WantedRoleDto $wantedRole */ ?>
@@ -21,55 +21,55 @@
                     <div class="item form-item">
                         <h4 class="form-item-title">役割名</h4>
                         <input type="text" class="input" name="party[wantedRoleList][{{$index}}][roleName]" value="{{ $wantedRole->roleName }}"><!-- 役割名 -->
-                        <input type="checkbox" name="party[wantedRoleList][{{$index}}][managerAssigned]" {{ $wantedRole->managerAssigned()? 'checked' : '' }}>この役職に自分も所属する
+                        <label class="label party-entry-organizer"><input type="checkbox" name="party[wantedRoleList][{{$index}}][managerAssigned]" {{ $wantedRole->managerAssigned()? 'checked' : '' }}>この役職に自分も所属する</label>
                         @foreach($errors->get("party.wantedRoleList.$index.roleName") as $message)
-                            <p>{{ $message }}</p>
+                            <p class="form-error">{{ $message }}</p>
                         @endforeach
                     </div><!-- item -->
                     <div class="item form-item">
                         <h4 class="form-item-title">参考ジョブ</h4>
-                        <select id="" name="party[wantedRoleList][{{$index}}][referenceJobId]"><!-- 参考ジョブ -->
+                        <select id="" class="input" name="party[wantedRoleList][{{$index}}][referenceJobId]"><!-- 参考ジョブ -->
                             <?php /* @var \App\Domain\Job\Job $job */ ?>
                             @foreach($allJob as $job)
                                 <option value="{{ $job->jobId()->code() }}" {{ $wantedRole->referenceJobId() == $job->jobId()->code()? 'selected' : '' }}>{{ $job->jobName() }}</option>
                             @endforeach
                         </select>
                         @foreach($errors->get("party.wantedRoleList.$index.referenceJobId") as $message)
-                            <p>{{ $message }}</p>
+                            <p class="form-error">{{ $message }}</p>
                         @endforeach
                     </div><!-- item -->
                     <div class="item form-item">
                         <h4 class="form-item-title">人数</h4>
-                        <input type="number" class="input input-large" name="party[wantedRoleList][{{$index}}][frameAmount]" value="{{ $wantedRole->frameAmount() }}"><!--　人数 -->
-                        <p>※自分も含む人数を記載してください</p>
+                        <input type="number" class="input" name="party[wantedRoleList][{{$index}}][frameAmount]" value="{{ $wantedRole->frameAmount() }}"><!--　人数 -->
+                        <p class="party-entry-organizer">※自分も含む人数を記載してください</p>
                         @foreach($errors->get("party.wantedRoleList.$index.frameAmount") as $message)
-                            <p>{{ $message }}</p>
+                            <p class="form-error">{{ $message }}</p>
                         @endforeach
                     </div><!-- item -->
                     <div class="item form-item">
                         <h4 class="form-item-title">備考</h4>
-                        <input type="textarea" class="input" name="party[wantedRoleList][{{$index}}][remarks]" value="{{ $wantedRole->remarks() }}"><!-- 備考 -->
+                        <textarea class="input textarea" name="party[wantedRoleList][{{$index}}][remarks]" value="{{ $wantedRole->remarks() }}"></textarea><!-- 備考 -->
                         @foreach($errors->get("party.wantedRoleList.$index.remarks") as $message)
-                            <p>{{ $message }}</p>
+                            <p class="form-error">{{ $message }}</p>
                         @endforeach
                     </div><!-- item -->
                 @endforeach
 
                 @foreach($errors->get("party.wantedRoleList") as $message)
-                    <p>{{ $message }}</p>
+                    <p class="form-error">{{ $message }}</p>
                 @endforeach
 
                 <div class="btn-wrap row flex-while flex-end-length">
-                    <button class="btn btn-back" type="submit">戻る</button>
+                    <button class="btn mod-gray" type="submit">戻る</button>
 
-                    <button class="btn btn-next"
+                    <button class="btn mod-orange"
                             type="submit"
                             name="handler"
                             value="{{ \App\Http\Requests\PartyRegistration\HandleWantedRequest::DONE }}">
                         確認
                     </button>
 
-                    <button class="btn btn-add"
+                    <button class="btn btn-add mod-blue"
                             type="submit"
                             name="handler"
                             value="{{ \App\Http\Requests\PartyRegistration\HandleWantedRequest::ADD }}">
