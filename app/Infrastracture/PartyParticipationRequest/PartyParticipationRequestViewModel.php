76ee4a4e31a760b76ae05a6383cf5f07967ad362
ymgn <b4183@oic.jp>
2018-01-14 15:23:52 +0900

    add PartyParticipationRequestViewModelを作成

---
+++ app/Infrastracture/PartyParticipationRequest/PartyParticipationRequestViewModel.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yamagon
+ * Date: 2018/01/14
+ * Time: 14:40
+ */
+
+namespace App\Infrastracture\PartyParticipationRequest;
+
+
+use App\Domain\GuildMember\RepositoryInterface\GuildMemberRepositoryInterface;
+use App\Domain\Party\RepositoryInterface\PartyRepositoryInterface;
+use App\Domain\PartyParticipationRequest\PartyParticipationRequest;
+use App\Infrastracture\GuildMember\GuildMemberViewModel;
+use App\Infrastracture\Party\PartyViewModel;
+use App\Infrastracture\WantedRole\WantedRoleViewModel;
+
+class PartyParticipationRequestViewModel
+{
+    private $partyParticipationRequest;
+    /* @var PartyRepositoryInterface $partyRepo */
+    private $partyRepo;
+    /* @var GuildMemberRepositoryInterface $guildMemberRepo */
+    private $guildMemberRepo;
+    private $party = null;
+    private $wantedRole = null;
+    private $guildMember = null;
+
+    public function __construct(PartyParticipationRequest $partyParticipationRequest)
+    {
+        $this->partyParticipationRequest = $partyParticipationRequest;
+        $this->id = $partyParticipationRequest->id();
+        $this->reply = $partyParticipationRequest->reply();
+        $this->partyRepo = app(PartyRepositoryInterface::class);
+        $this->guildMemberRepo = app(GuildMemberRepositoryInterface::class);
+    }
+
+    public function party(): PartyViewModel
+    {
+        if (is_null($this->party)){
+            $party = $this->partyRepo->findById($this->partyParticipationRequest->partyId());
+            $this->party = new PartyViewModel($party);
+        }
+
+        return $this->party;
+    }
+
+    public function wantedRole(): WantedRoleViewModel
+    {
+        if (is_null($this->wantedRole)){
+            $party = $this->party();
+            $result = array_values(array_filter($party->wantedRoles(), function (WantedRoleViewModel $wantedRole) {
+                if ($wantedRole->id === $this->partyParticipationRequest->wantedRoleId()){
+                    return $wantedRole;
+                };
+            }));
+            if(count($result) > 0) {
+                $this->wantedRole = $result[0];
+            }
+        }
+
+        return $this->wantedRole;
+    }
+
+    public function guildMember(): GuildMemberViewModel
+    {
+        if (is_null($this->guildMember)){
+            $guildMember = $this->guildMemberRepo->findByStudentNumber($this->partyParticipationRequest->guildMemberId());
+            $this->guildMember = new GuildMemberViewModel($guildMember);
+        }
+
+        return $this->guildMember;
+    }
+
+}
