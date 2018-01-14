847554db149cbf1bd54487b4f14d38c1a0390418
fuchi15 <sakuya815@gmail.com>
2018-01-14 18:02:32 +0900

    管理中のパーティー一覧 css　適用

--- resources/views/Guild/Party/Management/Holding.blade.php
+++ resources/views/Guild/Party/Management/Holding.blade.php
@@ -5,19 +5,43 @@
 @endsection
 
 @section('content')
-    @include('guild.party.management._navgation')
+<div class="background">
+    <div class="layer layer1 board"></div>
+</div><!-- background -->
+@include('guild.party.management._navgation')
+<div class="party-management">
     <a class="applicant-list-move">申請者一覧</a>
-    <ul class="party-management-list">
-      <li class="party-management-item">
-        <div class="party-management-holding-item-left">
-          <p class="party-management-holding-status">募集中</p>
-          <a href="#">編集</a>
-        </div>
-        <div class="party-management-holding-item-right">
-          <p class="party-management-holding-party-name">パーティー名</p>
-          <p class="party-management-holding-member">7/8人</p>
-          <button class="party-dissolution" type="button">解散</button>
-        </div>
-      </li>
+    <h2 class="party-management-title">管理中のパーティー一覧</h2>
+    <ul class="party-list party-management-list">
+        <li class="party-item party-management-item">
+            <div class="party-header">
+                <p class="party-type">Webシステム</p>
+                <h3 class="party-theme">学内の共同政策を推進するサービス</h3>
+            </div>
+            <div class="party-body row party-management-body">
+                <p class="party-body-left party-recruiting-status mod-yellow">募集中</p>
+                <div class="party-body-right">
+                    <div class="party-management-detail">
+                        <h4 class="party-management-body-title">役割</h4>
+                        <div class="party-management-body-content">
+                            <p>Webデザイナー</p>
+                            <p>希望ジョブ：Webデザイナー</p>
+                        </div>
+                    </div>
+                    <div class="party-management-detail marginb">
+                        <h4 class="party-management-body-title">活動期間</h4>
+                        <div class="party-management-body-content">
+                            <p>2017/11/10 ~ 2018/01/18<span>まで</span></p>
+                        </div>
+                    </div>
+                    <div class="btn-wrap row flex-while">
+                        <button class="btn mod-gray">解散</button>
+                        <button class="btn mod-light-green">詳細</button>
+                    </div>
+                </div>
+            </div>
+        </li>
     </ul>
+    </section>
+</div>
 @endsection
