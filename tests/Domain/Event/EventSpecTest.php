b83475662350da6c2be19b8595be4492b7d19470
ni-hara <quale7@gmail.com>
2018-02-04 07:54:48 +0900

    EventSpecとTestの作成

---
+++ tests/Domain/Event/EventSpecTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2018/02/04
+ * Time: 6:13
+ */
+
+namespace tests\Domain\Event;
+
+
+use App\Domain\Event\Event;
+use App\Domain\Event\Spec\EventSpec;
+use App\Domain\Event\ValueObjects\EvaluationPeriod;
+use App\Domain\Event\ValueObjects\EventHoldPeriod;
+use App\Domain\Event\ValueObjects\EventId;
+use App\Domain\Event\ValueObjects\ReleasePeriod;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class EventSpecTest extends TestCase
+{
+    /* @var Event $event */
+    private $event;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $id = new EventId('aaaa');
+        $name = 'MF';
+        $theme = 'oic';
+        $description = '作品展';
+        $releasePeriod = new ReleasePeriod('2018-01-01', '2018-02-01');
+        $eventHoldPeriod = new EventHoldPeriod('2018-01-02', '2018-01-15');
+        $evaluationPeriod = new EvaluationPeriod('2018-01-16', '2018-01-30');
+
+        $this->event = new Event(
+            $id,
+            $name,
+            $theme,
+            $description,
+            $releasePeriod,
+            $eventHoldPeriod,
+            $evaluationPeriod
+        );
+    }
+
+    public function testSuccess()
+    {
+        $this->assertTrue(EventSpec::allValidate($this->event));
+    }
+
+    public function testValidateWhetherWithinReleasePeriod()
+    {
+        $flag = true;
+        $date = new Carbon($this->event->eventHoldPeriod()->startDate()->format('Y-m-d'));
+        $interval = $this->event->releasePeriod()->endDate()->diff($this->event->releasePeriod()->startDate());
+
+        for ($i = 0; $i < $interval->days; $i++)
+        {
+            if(!EventSpec::validateWhetherWithinReleasePeriod(
+                $this->event->releasePeriod(),
+                new \DateTime($date))
+            ){$flag = false; break;}
+            $date = $date->addDay();
+        }
+
+        $this->assertTrue($flag);
+    }
+
+    public function testFailValidateWhetherWithinReleasePeriod()
+    {
+        $this->assertFalse(EventSpec::validateWhetherWithinReleasePeriod(
+            $this->event->releasePeriod(),
+            new \DateTime('2018-02-02')
+        ));
+    }
+
+    public function testValidateEndedEventHoldPeriod()
+    {
+        $this->assertTrue(EventSpec::validateEndedEventHoldPeriod(
+            $this->event->eventHoldPeriod(),
+            $this->event->evaluationPeriod()
+        ));
+    }
+
+    public function testFailValidateEndedEventHoldPeriod()
+    {
+        $this->assertFalse(EventSpec::validateEndedEventHoldPeriod(
+            $this->event->eventHoldPeriod(),
+            new EvaluationPeriod('2018-01-15', '2018-01-31')
+        ));
+    }
+}
