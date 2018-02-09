091a75dd805fc851abf417c1a81e4920f6f5e876
ymgn <b4183@oic.jp>
2018-02-09 17:21:45 +0900

    add Topページのお知らせの所にFeedを仮置き

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -8,8 +8,11 @@
 
 namespace App\Http\Controllers;
 
+use App\ApplicationService\FeedAppService;
+use App\Domain\Feed\Feed;
 use App\Domain\GuildMember\GuildMember;
 use App\Domain\Notification\RepositoryInterface\NotificationRepositoryInterface;
+use App\Infrastracture\Feed\FeedViewModel;
 use App\Infrastracture\GuildMember\GuildMemberViewModel;
 
 class TopController extends Controller
@@ -17,11 +20,19 @@ class TopController extends Controller
     // Top画面
     public function index(
         GuildMember $loginMember,
-        NotificationRepositoryInterface $notificationRepository
+        NotificationRepositoryInterface $notificationRepository,
+        FeedAppService $feedAppService
     )
     {
+        // 最新のFeedを取得
+        $feedList = $feedAppService->feed();
+        $feedViewModelList = array_map(function(Feed $feed) {
+            return new FeedViewModel($feed);
+        }, $feedList);
+
         return view('Top')
             ->with('guildMember', new GuildMemberViewModel($loginMember))
-            ->with('sendNotification', $notificationRepository->hasUnreadNotifications($loginMember->studentNumber()));
+            ->with('sendNotification', $notificationRepository->hasUnreadNotifications($loginMember->studentNumber()))
+            ->with('feedList', $feedViewModelList);
     }
 }
