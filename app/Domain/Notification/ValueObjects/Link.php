fc444db99ee0dbfc41d1f480e4e4fe95a82c24f8
ymgn <b4183@oic.jp>
2018-02-09 11:50:09 +0900

    delete LinkにあったURL作成メソッドを削除

--- app/Domain/Notification/ValueObjects/Link.php
+++ app/Domain/Notification/ValueObjects/Link.php
@@ -36,26 +36,4 @@ class Link
     {
         return $this->linkType;
     }
-
-    // LinkIdとタイプを元にURLを作成する
-    public function url()
-    {
-        if ($this->linkType->isPartyParticipationRequest()) {
-            return route('shoe_participation_request_list');
-        } elseif ($this->linkType->isParty()) {
-            return route('show_party_detail', ['partyId' => $this->linkToId]);
-        }
-    }
-
-    // LinkIdとタイプを元にパーティへのURLが作成出来る場合は作成して渡す
-    public function partyUrl()
-    {
-        if ($this->linkType->isParty()) {
-            return route('show_party_detail', ['partyId' => $this->linkToId]);
-        } elseif ($this->linkType->isPartyParticipationRequest()) {
-            $partyId = $this->partyParticipationRequestRepository->findById($this->linkToId())->partyId();
-            return route('show_party_detail', ['partyId' => $partyId]);
-        }
-        return null;
-    }
 }
