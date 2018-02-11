d5375bf4bb1af2847056999f3a8b59e9986d81d7
ymgn <b4183@oic.jp>
2018-02-11 15:36:13 +0900

    fix 未読通知の存在確認をNotificationRepositoryからNotificationSpecに変更

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -9,7 +9,7 @@
 namespace App\Http\Controllers;
 
 use App\Domain\GuildMember\GuildMember;
-use App\Domain\Notification\RepositoryInterface\NotificationRepositoryInterface;
+use App\Domain\Notification\Spec\NotificationSpec;
 use App\Infrastracture\GuildMember\GuildMemberViewModel;
 
 class TopController extends Controller
@@ -17,11 +17,11 @@ class TopController extends Controller
     // Top画面
     public function index(
         GuildMember $loginMember,
-        NotificationRepositoryInterface $notificationRepository
+        NotificationSpec $notificationSpec
     )
     {
         return view('Top')
             ->with('guildMember', new GuildMemberViewModel($loginMember))
-            ->with('sendNotification', $notificationRepository->hasUnreadNotifications($loginMember->studentNumber()));
+            ->with('sendNotification', $notificationSpec::hasUnreadNotifications($loginMember->studentNumber()));
     }
 }
