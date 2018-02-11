b87645de21981747fbbb1a544bfe43266c9f4688
ni-hara <quale7@gmail.com>
2018-02-12 07:18:20 +0900

    AppService実装

---
+++ app/ApplicationService/EventPartyAppService.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2018/02/12
+ * Time: 6:32
+ */
+
+namespace App\ApplicationService;
+
+
+use App\Domain\Event\ValueObjects\EventId;
+use App\Domain\EventParty\EventParty;
+use App\Domain\EventParty\EventPartyRepositoryInterface;
+use App\Domain\EventParty\Spec\EventPartySpec;
+
+class EventPartyAppService
+{
+    protected $repo;
+
+    public function __construct(EventPartyRepositoryInterface $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function joinEvent(EventId $eventId, string $partyId): bool
+    {
+        if(EventPartySpec::isExist($eventId, $partyId)) return false;
+
+        $eventParty = new EventParty($eventId, $partyId);
+        return $this->repo->save($eventParty);
+    }
+}
