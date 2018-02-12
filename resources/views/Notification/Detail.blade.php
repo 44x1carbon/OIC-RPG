05864f63cfde7e475f4d8b9bd8b92f58c5ce34e9
fuchi15 <sakuya815@gmail.com>
2018-02-12 20:45:55 +0900

    通知一覧と通知詳細のcssを追加

--- resources/views/Notification/Detail.blade.php
+++ resources/views/Notification/Detail.blade.php
@@ -5,19 +5,31 @@
 @endsection
 
 @section('content')
-    <div>
+<div class="background">
+    <div class="layer layer1 map"></div>
+    <div class="layer layer2 opasity"></div>
+</div><!-- background -->
+
+    <div class="notification">
         <?php /* @var \App\Infrastracture\Notification\NotificationViewModel $notification */ ?>
-        <div> {{ $notification->title() }}</div>
-        <div> {{ $notification->message() }} </div>
-            <a href="{{ route('show_notification') }}" class="notification-title">通知一覧</a>
+        <div class="notification-header"> 
+            <h2 class="notification-title">{{ $notification->title() }}</h2>
+        </div>
+        <div class="notification-body column flex-while">
+            <p class="message">{{ $notification->message() }}</p>
             @if($notification->notificationType()->is('receivePartyParticipationRequest'))
                 @if($notification->link()->linkType()->is('partyParticipationRequest'))
-                    <a href="{{ $notification->link()->url() }}" class="party">参加申請一覧</a>
+                <div class="row flex-center-length">
+                    <a href="{{ $notification->link()->url() }}" class="link">参加申請一覧を見る</a>
+                </div>
                 @endif
             @endif
+        </div>
             @if($notification->notificationType()->is('replyPartyParticipationRequest'))
                 @if($notification->link()->linkType()->is('partyParticipationRequest'))
-                    <a href="{{ $notification->link()->partyUrl() }}" class="party">パーティ</a>
+i               <div class="row flex-center-length">
+                    <a href="{{ $notification->link()->partyUrl() }}" class="link">パーティ詳細を見る</a>
+                </div>
                 @endif
             @endif
     </div>
--- resources/views/Notification/Index.blade.php
+++ resources/views/Notification/Index.blade.php
@@ -5,17 +5,32 @@
 @endsection
 
 @section('content')
-<div>
+<div class="background">
+    <div class="layer layer1" style="background: #333"></div>
+</div><!-- background -->
+<div class="notification">
+    <div class="notification-header">
+        <h2 class="notification-title">通知</h2>
+    </div>
     <?php /* @var \App\Infrastracture\Notification\NotificationViewModel $notification */ ?>
+    <ul class="notification-list">
     @foreach($notifications as $notification)
-        <a href="{{ route('show_notification_detail', ['notificationId' => $notification->id()]) }}" class="party">
-            <div class="title">
-                {{ $notification->title() }} {{$notification->isRead() ? '既読' : '未読'}}
-            </div>
-            <div class="notificationAt">
-                {{ $notification->notificationAt()->format('Y/m/d  H:i:s') }}
-            </div>
-        </a>
+        @if($notification->isRead())
+        <li class="notification-item is-read">
+        @else
+        <li class="notification-item">
+        @endif
+            <a href="{{ route('show_notification_detail', ['notificationId' => $notification->id()]) }}" class="party">
+                <div class="title">
+                    <p class="new"> {{$notification->isRead() ? '' : 'NEW'}}</p>
+                    <p>{{ $notification->title() }}</p>
+                </div>
+                <div class="notificationAt">
+                    {{ $notification->notificationAt()->format('Y/m/d  H:i:s') }}
+                </div>
+            </a>
+        </li>
     @endforeach
+    </ul>
 </div>
 @endsection
